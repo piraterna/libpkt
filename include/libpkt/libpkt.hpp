055ddfbde636135cfdcd2151574fbe928da2bba6
Kevin Alavik <kevin@alavik.se>
2025-07-12 11:57:03 +0200

    init

---
+++ include/libpkt/libpkt.hpp
@@ -0,0 +1,14 @@
+/*
+ * ============================================================================
+ * libpkt - A low-level C++ networking library for Linux.
+ * ============================================================================
+ * Apache License 2.0 (see LICENSE file or
+ * https://www.apache.org/licenses/LICENSE-2.0)
+ * ============================================================================
+ */
+#pragma once
+
+namespace libpkt
+{
+
+}
